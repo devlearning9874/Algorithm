6ae6510317a2c581011e36154cd8ada53b083139
devlearning9874 <devlearning9874@gmail.com>
2021-07-19 22:26:08 +0530

    Implemented Kadans Algo

--- kadans-algo/cpp/kadans-imp.cpp
+++ kadans-algo/cpp/kadans-imp.cpp
@@ -4,10 +4,31 @@
 
 using namespace std;
 
+int maxSumArray(int arr[], int n){
+     
+     int max_so_far = INT_MIN; 
+     int max_end_here = 0;
+
+     for(int i=0; i<n; i++){
+
+         max_end_here = max_end_here + arr[i];
+
+         if(max_so_far < max_end_here){
+             max_so_far = max_end_here;
+         }
+         if(max_end_here < 0)
+            max_end_here = 0;
+     }
+     return max_so_far;
+}
 
 int main(){
 
-    cout<<"Init Kadan's Algorithm";
+    int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
 
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int result = maxSumArray(arr,n);
+    cout<<result;
     return 0;
 }
